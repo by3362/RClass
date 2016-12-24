747707042a12d222edd120be63bd00e2d8272810
unknown <>
2016-12-25 02:04:31 +0800

    why???

--- RHw.R
+++ RHw.R
@@ -33,7 +33,7 @@ EightAndHalf_trouble
 write.table(EightAndHalf_3,"D:/EightAndHalf_3.csv",row.names=TRUE,col.names=TRUE,sep=",")
 a <- read.table("D:/EightAndHalf_3.csv",sep = ",",header = T)
 a
-
+ 
 #EightAndHalf_2與EightAndHalf_3得到的結果是一樣的,做到昏頭...
 #而跑圖只需要EightAndHalf_2與EightAndHalf_3即可
 EightAndHalf_Final <- ggplot(EightAndHalf_2, aes(x=縣市,y=入營總人數,fill=軍種類別)) + geom_bar(stat = "identity",colour="black") + ylab("總入營數") + xlab("縣市") + theme(axis.text.x=element_text(face = "bold",angle=45, vjust=.5,hjust=.5,size=8)) + labs(title = "104下半年國軍常備兵徵集配賦員額預告資料")+ scale_y_continuous(breaks=seq(0, 8000, 500))
